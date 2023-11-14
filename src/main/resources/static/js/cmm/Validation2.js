160d29e678a81a68c40ed0ccde51a3156384c288
gavy1004 <avanetkr22@gmail.com>
2023-11-14 17:54:19 +0900

    회원가입 UI 변경 중

--- src/main/resources/static/js/cmm/Validation2.js
+++ src/main/resources/static/js/cmm/Validation2.js
@@ -5,14 +5,16 @@ const MOBILE_NUMBER_REGEX = /^01([0|1|6|7|8|9]?)?([0-9]{3,4})?([0-9]{4})$/;
 
 class Validation {
     // class variables
-
     hasUserIdUniqueCheck = false;
+    hasEmailUniqueCheck = false;
     isPasswordValid = false;
     isPasswordsEqual = false;
     hasEmailValidated = false;
 
-    //step3
-    idInput;    // ID 
+    joinModal;
+    joinModalCanBtn;
+
+    idInput;        // ID 
     idDupCheckBtn;  // ID 중복 체크 
     idHelpMessage;
     passwordInput;
@@ -28,9 +30,6 @@ class Validation {
     emailConfirmNumberBtn;
     emailConfirmHelpMessage;
     emailReceptionAgreeCheckbox;
-    mobileInput;
-    snsReceptionAgreeCheckbox;
-    gotoStep4Btn;
 
     constructor() {
         this.idInput = document.getElementById("joinId");
@@ -48,12 +47,45 @@ class Validation {
         this.emailConfirmNumberBtn = document.getElementById("emailConfirmNumberBtn");
         this.emailConfirmHelpMessage = document.getElementById("emailConfirmHelpMessage");
         this.emailReceptionAgreeCheckbox = document.getElementById("emailReceptionAgreeCheckbox");
-
+        this.joinModal = document.getElementById("joinModal");
+        this.joinModalCanBtn = document.getElementById("joinModalCanBtn");
         this.initValidator();
         this.initUserIdCheck(); // ID 확인
         this.initInfoInputs();  // 인풋 입력
     }
 
+    init() {
+        const _this = this;
+        CustomAlert.confirm("창을 닫으시겠습니까? 창을 닫으면 입력내용이 초기화 됩니다.",function(){
+            $(_this.joinModal).modal('hide')	
+    
+            _this.hasUserIdUniqueCheck = false;
+            _this.hasEmailUniqueCheck = false;
+            _this.isPasswordValid = false;
+            _this.isPasswordsEqual = false;
+            _this.hasEmailValidated = false;
+    
+            document.forms['joinForm'].reset(); 
+    
+            _this.idHelpMessage.innerText = "";
+            _this.passwordHelpMessage.innerText = "";
+            _this.passwordHelpMessage2.innerText = "";
+    
+            _this.idInput.classList.remove("input-is-invalid");
+            _this.idInput.classList.remove("input-is-valid");
+            _this.passwordInput.classList.remove("input-is-invalid");
+            _this.passwordInput.classList.remove("input-is-valid");
+            _this.passwordCheckInput.classList.remove("input-is-invalid");
+            _this.passwordCheckInput.classList.remove("input-is-valid");
+            _this.emailConfirmDiv.style.display = 'none';
+
+            /*this.emailConfirmDiv = document.getElementById("emailConfirmDiv");
+            this.emailConfirmInput = document.getElementById("emailConfirmInput");
+            this.emailConfirmNumberBtn = document.getElementById("emailConfirmNumberBtn");
+            this.emailConfirmHelpMessage = document.getElementById("emailConfirmHelpMessage");
+            this.emailReceptionAgreeCheckbox = document.getElementById("emailReceptionAgreeCheckbox");*/
+        });
+    }
     initValidator() {
         const _this = this;
 
@@ -61,6 +93,9 @@ class Validation {
             await _this.sendEmailConfirmNumber();
         })
 
+        this.joinModalCanBtn.addEventListener('click', async (ev) => {
+            await _this.init();
+        })
     }
 
     initInfoInputs() {
@@ -206,10 +241,16 @@ class Validation {
     }
 
     async sendEmailConfirmNumber() {
+        /*if(!this.hasEmailUniqueCheck) {
+            CustomAlert.error("이메일을 확인해 주세요.");
+            return;
+        }*/
+
         if(!this.hasUserIdUniqueCheck) {
             CustomAlert.error("아이디 중복확인을 먼저 진행해주세요.");
             return;
         }
+
         //인증번호 보내기
         const _this = this;
         /*const urlFormat = "/signup/send-confirm-email?username={0}&name={1}&email={2}"
